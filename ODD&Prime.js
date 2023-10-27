f37783d294770dfaf5b40b52714b833ecd66ce4c
YUGANDHAR2003 <85959898+YUGANDHAR2003@users.noreply.github.com>
2023-10-27 15:01:03 +0530

    ODD&Prime.js

--- ODD&Prime.js
+++ ODD&Prime.js
@@ -4,12 +4,12 @@
  * @returns {number} - The count of digits in the input number.
  */
 function countDigits(inputNumber) {
-    let count = 0;
+    let count = 0
     while (inputNumber > 0) {
-        count++;
-        inputNumber = Math.floor(inputNumber / 10);
+        count++
+        inputNumber = Math.floor(inputNumber / 10)
     }
-    return count;
+    return count
 }
 
 /**
@@ -18,24 +18,24 @@ function countDigits(inputNumber) {
  * @returns {boolean} - True if the input number has the "ODD" property, otherwise false.
  */
 function isOddPropertyNumber(inputNumber) {
-    let initialCount = countDigits(inputNumber);
+    let initialCount = countDigits(inputNumber)
 
     if (initialCount === 1) {
-        return false; // Not ODD
+        return false // Not ODD
     }
 
     while (inputNumber > 0) {
-        let temp = inputNumber;
-        let rem1 = Math.floor(inputNumber % 10);
-        inputNumber = Math.floor(temp / 10);
-        let rem2 = Math.floor(inputNumber % 10);
+        let temp = inputNumber
+        let rem1 = Math.floor(inputNumber % 10)
+        inputNumber = Math.floor(temp / 10)
+        let rem2 = Math.floor(inputNumber % 10)
 
         if (Math.abs(rem1 - rem2) !== 1) {
-            return false; // Not ODD
+            return false // Not ODD
         }
     }
 
-    return true; // ODD
+    return true // ODD
 }
 
 /**
@@ -45,33 +45,33 @@ function isOddPropertyNumber(inputNumber) {
  */
 function isPrime(num) {
     if (num <= 1) {
-        return false;
+        return false
     }
 
     for (let i = 2; i <= Math.sqrt(num); i++) {
         if (num % i === 0) {
-            return false;
+            return false
         }
     }
 
-    return true;
+    return true
 }
 
 /**
  * Counts the number of ODD property prime numbers between 0 and 10 million.
  */
 function countOddPropertyPrimeNumbers() {
-    let count = 0;
+    let count = 0
 
     for (let i = 0; i <= 10000000; i++) {
         if (isOddPropertyNumber(i) && isPrime(i)) {
-            count++;
-            console.log("ODD property prime number: " + i);
+            count++
+            console.log("ODD property prime number: " + i)
         }
     }
 
-    console.log("Number of ODD property prime numbers between 0 and 10 million: " + count);
+    console.log("Number of ODD property prime numbers between 0 and 10 million: " + count)
 }
 
 // Call the function to count ODD property prime numbers
-countOddPropertyPrimeNumbers();
+countOddPropertyPrimeNumbers()
